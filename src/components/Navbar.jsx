75c894359ba6383c09da758dd98b2da7a72f1ca3
Your Name <you@example.com>
2023-03-07 04:46:47 -0400

    home ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About me</a>
+        <a href="#">Skills</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
